5c27400f6b2e40a23e0dd3afd68ba29b14afe73b
Alex Goodwin <cyberia.resurrection@gmail.com>
2019-07-15 03:42:36 +1000

    Deconflict index names

--- database/migrations/2017_02_25_014714_create_purchase_order_details_table.php
+++ database/migrations/2017_02_25_014714_create_purchase_order_details_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePurchaseOrderDetailsTable extends Migration {
 
@@ -15,13 +16,13 @@ class CreatePurchaseOrderDetailsTable extends Migration {
 		Schema::create('purchase_order_details', function(Blueprint $table)
 		{
 			$table->integer('id', true);
-			$table->integer('purchase_order_id')->index('purchase_order_id_2');
-			$table->integer('product_id')->nullable()->index('product_id_2');
+			$table->integer('purchase_order_id')->index('purchase_order_details_id_2');
+			$table->integer('product_id')->nullable()->index('purchase_order_details_product_id_2');
 			$table->decimal('quantity', 18, 4);
 			$table->decimal('unit_cost', 19, 4);
 			$table->dateTime('date_received')->nullable();
 			$table->boolean('posted_to_inventory')->default(0);
-			$table->integer('inventory_id')->nullable()->index('inventory_id_2');
+			$table->integer('inventory_id')->nullable()->index('purchase_order_details_inventory_id_2');
 		});
 	}
 
